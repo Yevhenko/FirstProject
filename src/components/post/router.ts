d4c5dda442ec0bd780d51f2dfcbb4e3261e2cedd
Yevhen Svyrydov <yevhenkozlyi@gmail.com>
2021-06-07 10:36:23 +0300

    refactor completed

--- src/components/post/router.ts
+++ src/components/post/router.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import { validateRequest } from '@utils/requestValidation';
 import { getQuerySchema, paramsSchema } from '@utils/schemas';
-import { postSchemas } from '@components/post';
+import { createPostSchema, updatePostSchema } from '@components/post/schemas';
 import { createPost, deletePost, getAllPosts, getOnePost, updatePost } from './controller';
 
 export const post = express.Router();
 
 post
-  .post('/posts', validateRequest(postSchemas.createPostSchema), createPost)
+  .post('/posts', validateRequest(createPostSchema), createPost)
   .get('/posts', validateRequest(getQuerySchema), getAllPosts)
   .get('/posts/:id', validateRequest(paramsSchema), getOnePost)
   .delete('/posts/:id', validateRequest(paramsSchema), deletePost)
-  .patch('/posts/:id', validateRequest(postSchemas.updatePostSchema), updatePost);
+  .patch('/posts/:id', validateRequest(updatePostSchema), updatePost);
